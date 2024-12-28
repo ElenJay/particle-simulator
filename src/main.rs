c29d9382a6345db421276106a78788fe4e449936
ElenJay <ihorlutsiuk@gmail.com>
2024-12-28 18:52:08 +0200

    Add gravity to particles, add window boundary constraints

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,9 @@ use particle::Particle;
 const WINDOW_WIDTH: i32 = 1600;
 const WINDOW_HEIGHT: i32 = 900;
 
+const GRAVITY: f32 = 9.8;
+const TIME_STEP: f32 = 0.1;
+
 fn main() {
     let (mut rl, thread) = raylib::init()
         .size(WINDOW_WIDTH, WINDOW_HEIGHT)
@@ -15,14 +18,20 @@ fn main() {
         .build();
 
     let mut particles: Vec<Particle> = Vec::with_capacity(10);
-    particles.push(Particle::new(WINDOW_WIDTH as f32 / 2.0, WINDOW_HEIGHT as f32 / 2.0));
+    particles.push(Particle::new(WINDOW_WIDTH as f32 / 2.0 - 50.0, WINDOW_HEIGHT as f32 / 2.0 - 50.0));
+    particles.push(Particle::new(WINDOW_WIDTH as f32 / 2.0 - 50.0, WINDOW_HEIGHT as f32 / 2.0 + 50.0));
+    particles.push(Particle::new(WINDOW_WIDTH as f32 / 2.0 + 50.0, WINDOW_HEIGHT as f32 / 2.0 - 50.0));
+    particles.push(Particle::new(WINDOW_WIDTH as f32 / 2.0 + 50.0, WINDOW_HEIGHT as f32 / 2.0 + 50.0));
 
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
         d.clear_background(Color::BLACK);
 
-        for item in &particles {
-            d.draw_circle_v(item.get_position(), 20.0, Color::WHITE);
+        for item in &mut particles {
+            item.apply_force(Vector2::new(0.0, GRAVITY));
+            item.update(TIME_STEP);
+            item.constraint_to_bounds(WINDOW_WIDTH as f32, WINDOW_HEIGHT as f32);
+            d.draw_circle_v(item.get_position(), item.get_radius(), Color::WHITE);
         }
     }
 }
--- src/particle.rs
+++ src/particle.rs
@@ -1,9 +1,12 @@
 use raylib::prelude::*;
 
+const PARTICLE_RADIUS: f32 = 15.0;
+
 pub struct Particle {
     pos: Vector2,
     prev_pos: Vector2,
     accl: Vector2,
+    radius: f32,
 }
 
 impl Particle {
@@ -11,11 +14,35 @@ impl Particle {
         Self {
             pos: Vector2::new(x, y),
             prev_pos: Vector2::new(x, y),
-            accl: Vector2::new(x, y),
+            accl: Vector2::new(0.0, 0.0),
+            radius: PARTICLE_RADIUS,
         }
     }
 
     pub fn get_position(&self) -> Vector2 {
         self.pos
+    }
+
+    pub fn get_radius(&self) -> f32 {
+        self.radius
+    }
+
+    pub fn apply_force(&mut self, force: Vector2) {
+        self.accl += force;
+    }
+
+    pub fn update(&mut self, time_step: f32) {
+        // Verlet integration
+        let velocity: Vector2 = self.pos - self.prev_pos;
+        self.prev_pos = self.pos;
+        self.pos += velocity + self.accl * time_step * time_step;
+        (self.accl.x, self.accl.y) = (0.0, 0.0);
+    }
+
+    pub fn constraint_to_bounds(&mut self, width: f32, height: f32) {
+        if self.pos.x < self.radius { self.pos.x = self.radius }
+        if self.pos.x > width - self.radius { self.pos.x = width - self.radius }
+        if self.pos.y < self.radius { self.pos.y = self.radius }
+        if self.pos.y > height - self.radius { self.pos.y = height - self.radius }
     }
 }
